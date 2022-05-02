3812c68c2f5a012f0ddab3493d1316ed3d0c3270
krome0 <sealminc@gmail.com>
2022-05-03 00:57:49 +0900

    Create(1931.py)

---
+++ python/silver1/1931.py
@@ -0,0 +1,15 @@
+n = int(input())
+timelist = []
+for _ in range(n):
+    starttime, endtime = map(int, input().split())
+    timelist.append((starttime, endtime))
+
+timelist.sort(key=lambda x: (x[1], x[1]-x[0]))
+
+endtime_buff = 0
+count = 0
+for starttime, endtime in timelist:
+    if starttime >= endtime_buff:
+        endtime_buff = endtime
+        count += 1
+print(count)
