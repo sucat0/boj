9c39005f41f66266cf721ec39aec8365dd0ccb58
krome0 <sealminc@gmail.com>
2022-05-03 01:14:54 +0900

    Fix timelist.sort's key to -(x[1], -(x[1]-x[0]))

--- python/silver1/1931.py
+++ python/silver1/1931.py
@@ -4,7 +4,7 @@ for _ in range(n):
     starttime, endtime = map(int, input().split())
     timelist.append((starttime, endtime))
 
-timelist.sort(key=lambda x: (x[1], x[1]-x[0]))
+timelist.sort(key=lambda x: (x[1], -(x[1]-x[0])))
 
 endtime_buff = 0
 count = 0
