9fbd4d22e98e94a8bafbac46edb0edd17900ede9
krome0 <sealminc@gmail.com>
2022-05-03 19:11:40 +0900

    Create(1036.c) but not completed

---
+++ c/gold1/1036.c
@@ -0,0 +1,15 @@
+// 아직 c가 미숙해서 파이썬으로 다시 구현할 예정
+#include <stdio.h>
+#include <stdlib.h>
+
+int cov36()
+
+int main(void){
+    int n, k;
+    scanf("%d", &n);  // N입력
+    int nums[n];
+    for(int count=0; count<n; ++count){
+        scanf("%d", &nums[count]);  // 36진수 수 입력
+    }
+    scanf("%d", &k);  // K입력
+}
