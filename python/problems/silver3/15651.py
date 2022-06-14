55f7b4189fe91ad08a0992a11bfe45e99c56f506
Krome <sealminc@gmail.com>
2022-06-15 03:17:10 +0900

    Solve 15651

---
+++ python/problems/silver3/15651.py
@@ -0,0 +1,18 @@
+def combination(n, r, current_index: int = 0, result_list: list = []):
+    if current_index == r:
+        yield ' '.join(map(str, result_list))
+
+    else:
+        for num in range(1, n+1):
+            yield from combination(n, r, current_index+1, result_list+[num])
+
+
+def main():
+    n, m = map(int, input().split())
+
+    for nums in combination(n, m):
+        print(nums)
+
+
+if __name__ == '__main__':
+    main()
