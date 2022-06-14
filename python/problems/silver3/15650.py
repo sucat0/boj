1986944f9cb6a50641e75a2d223266df50913553
Krome <sealminc@gmail.com>
2022-06-15 01:40:35 +0900

    Rename permutation to combination

--- python/problems/silver3/15650.py
+++ python/problems/silver3/15650.py
@@ -1,15 +1,15 @@
 n, m = map(int, input().split())
 
 
-def permutation(n, r, current_index: int = 0, result_list: list = []):
+def combination(n, r, current_index: int = 0, result_list: list = []):
     if current_index == m:
         yield ' '.join(map(str, result_list))
 
     else:
         for num in range(1, n+1):
             if current_index == 0 or num > result_list[current_index-1]:
-                yield from permutation(n, r, current_index+1, result_list+[num])
+                yield from combination(n, r, current_index+1, result_list+[num])
 
 
-for nums in permutation(n, m):
+for nums in combination(n, m):
     print(nums)
