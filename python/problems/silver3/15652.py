4249f682a652949c11878f45bba2cbcbc5547ee9
Krome <sealminc@gmail.com>
2022-06-15 01:42:38 +0900

    Rename m to r

--- python/problems/silver3/15652.py
+++ python/problems/silver3/15652.py
@@ -1,15 +1,15 @@
 n, m = map(int, input().split())
 
 
-def combination(n, r, current_index: int = 0, result_list: list = []):
+def allow_same_combination(n, r, current_index: int = 0, result_list: list = []):
     if current_index == r:
         yield ' '.join(map(str, result_list))
 
     else:
         for num in range(1, n+1):
             if current_index == 0 or num > result_list[current_index-1]:
-                yield from combination(n, r, current_index+1, result_list+[num])
+                yield from allow_same_combination(n, r, current_index+1, result_list+[num])
 
 
-for nums in combination(n, m):
+for nums in allow_same_combination(n, m):
     print(nums)
