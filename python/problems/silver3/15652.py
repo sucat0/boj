f717971d74f4a5814c9138ca156abe57aad5c8c7
Krome <sealminc@gmail.com>
2022-06-15 01:47:10 +0900

    Solve 15652

--- python/problems/silver3/15652.py
+++ python/problems/silver3/15652.py
@@ -1,15 +1,19 @@
-n, m = map(int, input().split())
-
-
-def allow_same_combination(n, r, current_index: int = 0, result_list: list = []):
+def combination(n, r, current_index: int = 0, result_list: list = []):
     if current_index == r:
         yield ' '.join(map(str, result_list))
 
     else:
         for num in range(1, n+1):
-            if current_index == 0 or num > result_list[current_index-1]:
-                yield from allow_same_combination(n, r, current_index+1, result_list+[num])
+            if current_index == 0 or num >= result_list[current_index-1]:
+                yield from combination(n, r, current_index+1, result_list+[num])
 
 
-for nums in allow_same_combination(n, m):
-    print(nums)
+def main():
+    n, m = map(int, input().split())
+
+    for nums in combination(n, m):
+        print(nums)
+
+
+if __name__ == '__main__':
+    main()
