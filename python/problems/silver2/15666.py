81fb35ff0866cd30433d54d269d142817950a249
krome0 <sealminc@gmail.com>
2022-06-17 14:39:47 +0900

    Solve 15666

---
+++ python/problems/silver2/15666.py
@@ -0,0 +1,25 @@
+def combination(n, r, n_list, current_index=0, *result):
+    if current_index == r:
+        yield ' '.join(map(str, result))
+
+    else:
+        prev_num = 0
+        for num_index in range(n):
+            num = n_list[num_index]
+
+            if num != prev_num and (current_index == 0 or num >= result[current_index-1]):
+                prev_num = num
+
+                yield from combination(n, r, n_list, current_index+1, *result, num)
+
+
+def main():
+    n, m = map(int, input().split())
+    n_list = sorted(list(map(int, input().split())))
+
+    for nums in combination(n, m, n_list):
+        print(nums)
+
+
+if __name__ == '__main__':
+    main()
