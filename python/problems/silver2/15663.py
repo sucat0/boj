356c123c6fc0744f04dc651d1faab96c5c24d5a0
krome0 <sealminc@gmail.com>
2022-06-17 14:36:25 +0900

    Solve 15663

---
+++ python/problems/silver2/15663.py
@@ -0,0 +1,31 @@
+def combination(n, r, n_list, current_index=0, visited=None, *result):
+    if visited is None:
+        visited = [False] * n
+
+    if current_index == r:
+        yield ' '.join(map(str, result))
+
+    else:
+        prev_num = 0
+        for num_index in range(n):
+            num = n_list[num_index]
+
+            if num != prev_num and (current_index == 0 or not visited[num_index]):
+                visited[num_index] = True
+                prev_num = num
+
+                yield from combination(n, r, n_list, current_index+1, visited, *result, num)
+
+                visited[num_index] = False
+
+
+def main():
+    n, m = map(int, input().split())
+    n_list = sorted(list(map(int, input().split())))
+
+    for nums in combination(n, m, n_list):
+        print(nums)
+
+
+if __name__ == '__main__':
+    main()
